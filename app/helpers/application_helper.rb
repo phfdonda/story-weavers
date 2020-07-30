c7a66ec37e70216aefb6d8110a15d3f9bf3061dc
Pedro Donda <phfdonda@gmail.com>
2020-07-29 21:56:26 -0300

    fix logged_in_as method routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,11 +22,12 @@ module ApplicationHelper
 
   def logged_in_as
     if logged_in?
-      render welcome_user
-      link_to 'Edit profile', edit_user_registration_path, class: 'navbar-link'
-      link_to 'Logout', destroy_user_session_path, method: :delete, class: 'navbar-link'
-    elsif link_to 'Sign up', new_user_registration_path, class: 'navbar-link'
-      link_to 'Login', new_user_session_path, class: 'navbar-link'
+      render 'layouts/welcome_user'
+      # link_to 'Edit profile', edit_user_registration_path, class: 'navbar-link'
+      link_to 'Logout', logout_path, method: :delete, class: 'navbar-link'
+    else
+      link_to 'Register', signup_path, class: 'navbar-link'
+      link_to 'Log In', login_path, class: 'navbar-link'
     end
   end
 end
