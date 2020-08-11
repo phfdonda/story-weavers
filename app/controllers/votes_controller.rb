14308f949d962fc8789440081dfe0d8c51ea62c2
Pedro Donda <phfdonda@gmail.com>
2020-08-11 15:36:36 -0300

    fix unvote path

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -12,9 +12,12 @@ class VotesController < ApplicationController
     vote = Vote.find_by(user_id: current_user.id, article_id: params[:id])
     @article = Article.find_it(params[:id])
     if vote&.destroy
+      unvote_phrase = "I guess '#{@article.title}' didn't deserve your vote, huh?"
       @article.n_of_votes -= 1
       @article.save
-      redirect_to category_show_path, notice: "I guess '#{@article.title}' didn't deserve your vote, huh?"
+      redirect_to "/category/show/#{@article.category_id}", notice: unvote_phrase
+    else
+      redirect to root_path, alert: 'What? You tried to unvote? How rude!'
     end
   end
 end
