087c2523b2b9b0ec2cf225e98349d12cd20ffbc3
Pedro Donda <phfdonda@gmail.com>
2020-09-03 11:32:51 -0300

    tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+let environment = {
   plugins: [
+    require('tailwindcss')('./app/javascript/css/tailwind.config.js'),
+    require('autoprefixer'),
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
@@ -9,4 +11,20 @@ module.exports = {
       stage: 3
     })
   ]
+};
+
+if (process.env.ENV_RAILS === 'production'){
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')(
+      {
+        content:[
+          './app/**/.html.erb',
+          './app/helpers/**/*.rb'
+        ],
+        defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+      }
+    )
+  )
 }
+
+module.exports = environment;
